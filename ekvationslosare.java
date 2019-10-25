befc773321aa11406dd5f4ab36a351d890ed56dc
krumilurfigur <gabrieloberg3@gmail.com>
2019-10-25 11:19:18 +0200

    end of the lession

---
+++ ekvationslosare.java
@@ -0,0 +1,17 @@
+/*
+A program that solves 0, 1 and 2nd degree polynomials
+2019-10-25
+Gabriel Öberg
+ */
+
+import java.util.Scanner;
+
+public class NAME {
+    public static void main(String[] args) {
+        scanner kb = new scanner(System.in);
+        System.out.println("What grade is your polynomial?");
+        g = kb.nextInt();
+
+        if g
+    }
+}
